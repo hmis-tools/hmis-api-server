2edd50044e30af7975ccb54c9799265f94f6d5b4
slifty <slifty@gmail.com>
2015-07-13 11:24:33 -0400

    added migration to healthcheck

--- src/main/java/org/openhmis/webservice/impl/HealthCheck.java
+++ src/main/java/org/openhmis/webservice/impl/HealthCheck.java
@@ -22,4 +22,31 @@ public class HealthCheck {
     public String healthcheck() {
         return "Your service is working.";
     }
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/migrate")
+    public String migrate() {
+
+        // Migrations require a session to work
+        if(System.getenv("DATABASE_URL") != null) {
+            Flyway flyway = new Flyway();
+            URI envDbUri = new URI(System.getenv("DATABASE_URL"));
+            String username = envDbUri.getUserInfo().split(":")[0];
+            String password = envDbUri.getUserInfo().split(":")[1];
+            String dbUrl = "jdbc:" + envDbUri.getScheme() + "://" + envDbUri.getHost() + ':' + envDbUri.getPort() + envDbUri.getPath();
+
+            flyway.setDataSource(dbUrl, username, password);
+            flyway.migrate();
+            return "Done."
+        } else {
+            return "Not Configured."
+        }
+    }
 }
