08b4975eedac1fcb118f28cc3aed80a58696a9f8
Dan Schultz <slifty@gmail.com>
2016-05-03 22:08:23 -0400

    Added XMLSeeAlso patch to single item DTO as well

--- src/main/java/org/openhmis/dto/result/DataResultDTO.java
+++ src/main/java/org/openhmis/dto/result/DataResultDTO.java
@@ -17,8 +17,33 @@ import com.fasterxml.jackson.annotation.JsonRootName;
 
 @JsonRootName("data")
 @XmlRootElement(name = "data")
+// TODO: This is nasty, but I need to do it for now pending further research
+// When translating to XML things break if we don't explicitly reference these
+// This started when adding the WriterInterceptor.
 @XmlSeeAlso({
+	org.openhmis.dto.ChronicHealthConditionDTO.class,
 	org.openhmis.dto.ClientDTO.class,
+	org.openhmis.dto.CoCDTO.class,
+	org.openhmis.dto.ContactDTO.class,
+	org.openhmis.dto.DevelopmentalDisabilityDTO.class,
+	org.openhmis.dto.DomesticAbuseDTO.class,
+	org.openhmis.dto.EnrollmentDTO.class,
+	org.openhmis.dto.ExitDTO.class,
+	org.openhmis.dto.FinancialAssistanceDTO.class,
+	org.openhmis.dto.FunderDTO.class,
+	org.openhmis.dto.HealthInsuranceDTO.class,
+	org.openhmis.dto.HivAidsStatusDTO.class,
+	org.openhmis.dto.IncomeSourceDTO.class,
+	org.openhmis.dto.InventoryDTO.class,
+	org.openhmis.dto.MedicalAssistanceDTO.class,
+	org.openhmis.dto.MentalHealthProblemDTO.class,
+	org.openhmis.dto.NonCashBenefitDTO.class,
+	org.openhmis.dto.PhysicalDisabilityDTO.class,
+	org.openhmis.dto.ProjectDTO.class,
+	org.openhmis.dto.ReferralDTO.class,
+	org.openhmis.dto.ServiceDTO.class,
+	org.openhmis.dto.SiteDTO.class,
+	org.openhmis.dto.SubstanceAbuseDTO.class,
 })
 public class DataResultDTO {
 
