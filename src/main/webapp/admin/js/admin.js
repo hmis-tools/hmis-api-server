4e57d4e5545359e41dcb7b37cca8d195d6f04019
Dan Schultz <slifty@gmail.com>
2016-05-09 00:04:55 -0400

    Initial administrative pages

---
+++ src/main/webapp/admin/js/admin.js
@@ -0,0 +1,50 @@
+$(function() {
+
+
+	// Activate login
+	$("#login").click(function() {
+		if(auth2)
+			auth2.grantOfflineAccess({'redirect_uri': 'postmessage'}).then(signInCallback);
+		else
+			$("body").html("There was a problem setting up Google OAuth.");
+
+	});
+
+
+})
+
+
+// Called by google
+function start() {
+	// Initialize google oauth
+	gapi.load('auth2', function() {
+		var clientId = $.get("../api/v3/authenticate/google/client_key", function(data) {
+			auth2 = gapi.auth2.init({
+				client_id: data,
+			});
+		})
+	});
+}
+
+// Called by google
+function signInCallback(authResult) {
+	if (authResult['code']) {
+		// Hide the sign-in button now that the user is authorized, for example:
+		$('#login').hide();
+
+		// Send the code to the server to get a token back
+		$.ajax({
+			"type": "POST",
+			"url": "../api/v3/authenticate/google",
+			"data": authResult['code'],
+			"processData": false,
+			"dataType": "json"
+		}).success(function(data) {
+			var idToken = data["id_token"];
+			console.log(idToken);
+		});
+	} else {
+		// There was an error.
+		$("body").html("There was a problem logging in, please refresh and try again.");
+	}
+}
